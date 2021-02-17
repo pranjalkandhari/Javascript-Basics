dba2a95b8d129036aabdef8f687d182cc31516cf
pranjalkandhari <pranjalkandhari@gmail.com>
2021-02-17 15:08:55 +0530

    Added basic command to scrap data from web page

--- 09DOM/DOM.js
+++ 09DOM/DOM.js
@@ -1,1 +1,8 @@
 //DOM: Document Object Model.
+
+//How to grab web elements in javascript?:
+
+//These lines where written in the console:
+var title = document.getElementsByTagName("h1")[0].innerHTML;
+console.log(title);
+//Output: LearnCodeOnline.in
