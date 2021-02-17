e75eacd1d1e027fb910ef2c8345506685ae2f1ce
pranjalkandhari <pranjalkandhari@gmail.com>
2021-02-17 21:20:44 +0530

    Added JS code to change color by hovering on an area

--- 09DOM/getComputedProperties/script.js
+++ 09DOM/getComputedProperties/script.js
@@ -6,12 +6,12 @@ const violet = document.querySelector(".violet");
 const orange = document.querySelector(".orange");
 const pink = document.querySelector(".pink");
 
-const centre = document.querySelector(".centre");
+var center = document.querySelector(".center");
 
 //Now how do I get the background color from the query?
 //We use properties of the Window object:
 //getComputedStyle: It take a element (here: returned by query selector).
-console.log(window.getComputedStyle(red));
+console.log(window.getComputedStyle(center));
 //The above line gives all properties 
 //How to find a specific property:
 //If in CSS: background-color then getComputedStyle will have property: backgroundColor.
@@ -29,3 +29,34 @@ console.log("cyan:",getBGColor(cyan));
 console.log("violet:",getBGColor(violet));
 console.log("orange:",getBGColor(orange));
 console.log("pink:",getBGColor(pink));
+//console.log("CENTER:",getBGColor(center));
+
+//EVENT Listeners: Records the event and performs an action 
+//Event is the 1st argument
+//(action is passed as 2nd argument as a function).
+//eg: mouseentre tells when a pointing device enters a specific area.
+//click: event of clicking
+
+var pinkColor = getBGColor(pink);
+//pink or any other color will actually refect of the query.
+pink.addEventListener("click" , () => {
+   center.style.backgroundColor = pinkColor; //style will help us 'change' the color unlike getComputedStyle.
+})
+
+var orangeColor  = getBGColor(orange);
+orange.addEventListener("mouseenter" , () => {
+    center.style.backgroundColor = orangeColor;
+})
+
+//Now lets make a generic function for all colors:
+//But how? 
+//We will return a function call to the window in another function :D
+const magicColorChange = (element , color) => {
+    return element.addEventListener("mouseenter" , () => {
+        center.style.backgroundColor = color;
+    });
+}
+
+magicColorChange(red , getBGColor(red));
+magicColorChange(violet , getBGColor(violet));
+magicColorChange(cyan , getBGColor(cyan));
